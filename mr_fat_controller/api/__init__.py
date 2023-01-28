09b93f8e7064fc0a8ac693d75094461fc99aefa7
Mark Hall <mark.hall@work.room3b.eu>
2023-01-28 17:55:07 +0000

    Check the database connection before reporting the status

--- mr_fat_controller/api/__init__.py
+++ mr_fat_controller/api/__init__.py
@@ -1,8 +1,11 @@
 """Server routes."""
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import controllers
+from ..models import db_session
 
 
 router = APIRouter(prefix='/api')
@@ -17,6 +20,10 @@ class StatusModel(BaseModel):
 
 
 @router.get('/status', response_model=StatusModel)
-def status() -> dict:
+async def status(dbsession: AsyncSession = Depends(db_session)) -> dict:
     """Return the current server status."""
+    try:
+        await dbsession.execute(text('SELECT * FROM alembic_version'))
+    except Exception:
+        return {'ready': False}
     return {'ready': True}
