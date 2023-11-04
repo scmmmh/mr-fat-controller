db61020a4c4cdb94496f5b8a7d58e43105cbf0db
Mark Hall <mark.hall@work.room3b.eu>
2023-11-04 11:04:08 +0000

    Added template frontend app

--- mr_fat_controller/server.py
+++ mr_fat_controller/server.py
@@ -1,7 +1,16 @@
 """The core MR Fat Controller server."""
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 
 from mr_fat_controller import api
 
 app = FastAPI()
 app.include_router(api.router)
+app.mount("/app", StaticFiles(packages=[("mr_fat_controller", "frontend/dist")], html=True), name="app")
+
+
+@app.get("/", response_class=RedirectResponse)
+def root() -> str:
+    """Redirect to the frontend application."""
+    return "/app"
