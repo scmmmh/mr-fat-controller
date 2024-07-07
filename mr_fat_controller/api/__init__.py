7a2a93513a9bb8e489c0c0326f3441fb01d7b3c9
Mark <mark.hall@work.room3b.eu>
2024-07-07 23:11:47 +0100

    Update style workflow (#10)
    * Update style workflow
    
    * Use the correct dependency function

--- mr_fat_controller/api/__init__.py
+++ mr_fat_controller/api/__init__.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from mr_fat_controller.models import db_session
+from mr_fat_controller.models import inject_db_session
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/api")
@@ -21,7 +21,7 @@ class StatusModel(BaseModel):
 
 
 @router.get("/status", response_model=StatusModel)
-async def status(dbsession: AsyncSession = Depends(db_session)) -> dict:
+async def status(dbsession: AsyncSession = Depends(inject_db_session)) -> dict:
     """Return the current server status."""
     try:
         await dbsession.execute(text("SELECT * FROM alembic_version"))
