ca3070f504ebc235bf9939ff6bab13a1277f9b25
Mark Hall <mark.hall@work.room3b.eu>
2025-01-03 09:28:27 +0000

    Added disconnected alert

--- mr_fat_controller/frontend/src/lib/StateSocketProvider.svelte
+++ mr_fat_controller/frontend/src/lib/StateSocketProvider.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { AlertDialog } from "bits-ui";
   import { setContext } from "svelte";
   import { writable } from "svelte/store";
 
   const state = writable({ points: {}, power_switch: {} } as State);
+  let disconnected = true;
   setContext("state", state);
   let ws: WebSocket | null = null;
 
@@ -11,6 +13,7 @@
 
     ws = new WebSocket("/api/state");
     ws.addEventListener("message", (ev: MessageEvent) => {
+      disconnected = false;
       const msg = JSON.parse(ev.data) as StateMessage;
       if (msg.type === "state") {
         const new_state = { points: {}, power_switch: {} } as State;
@@ -23,7 +26,9 @@
         state.set(new_state);
       }
     });
+
     ws.addEventListener("close", () => {
+      disconnected = true;
       window.setTimeout(connect, 10000);
     });
   }
@@ -38,5 +43,28 @@
 
   connect();
 </script>
+
+<AlertDialog.Root
+  bind:open={disconnected}
+  closeOnEscape={false}
+  closeOnOutsideClick={false}
+>
+  <AlertDialog.Portal>
+    <AlertDialog.Overlay class="fixed inset-0 z-50 bg-white/80" />
+    <AlertDialog.Content
+      class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-96 flex flex-col bg-white border-2 border-black rounded-lg shadow-lg overflow-hidden"
+    >
+      <AlertDialog.Title class="px-4 py-2 border-b-2 border-black font-bold"
+        >Disconnected</AlertDialog.Title
+      >
+      <AlertDialog.Description class="px-4 py-2">
+        <p>
+          You are currently disconnected from the server. The connection will be
+          re-established automatically. Please wait.
+        </p>
+      </AlertDialog.Description>
+    </AlertDialog.Content>
+  </AlertDialog.Portal>
+</AlertDialog.Root>
 
 <slot />
