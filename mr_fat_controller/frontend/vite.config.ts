0517cb29c4e5308958e62611fdbc1ec2a45adb97
Mark Hall <mark.hall@work.room3b.eu>
2025-01-28 19:50:51 +0000

    Transition to tailwindcss 4

--- mr_fat_controller/frontend/vite.config.ts
+++ mr_fat_controller/frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/app',
+  plugins: [svelte(), tailwindcss()],
+  base: "/app",
 })
